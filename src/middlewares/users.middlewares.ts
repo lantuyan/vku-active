ffe14448326c6b08cc003965cef21ade6deea3cc
lantuyan <nqlan.20it4@vku.udn.vn>
2023-06-21 02:19:15 +0700

    create funtion registerValidator

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { checkSchema } from 'express-validator';
+import { validate } from '~/utils/validation';
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
@@ -9,3 +11,91 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   }
   next();
 };
+
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 3,
+          max: 100
+        }
+      },
+      trim: true
+    },
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true
+    },
+    msv: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 20
+        }
+      },
+      trim: true
+    },
+    password: {
+      notEmpty: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 100
+        }
+      },
+      isStrongPassword: {
+        errorMessage:
+          'Password must be at least 8 characters long and contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol',
+        options: {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      }
+    },
+    confirm_password: {
+      notEmpty: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 100
+        }
+      },
+      isStrongPassword: {
+        errorMessage:
+          'Password must be at least 8 characters long and contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol',
+        options: {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      custom: {
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Password confirmation is incorrect');
+          }
+          return true;
+        }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
+      }
+    }
+  })
+);
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,9 +2,23 @@
 // const usersRouter = express.Router();
 import { Router } from 'express';
 import { loginController, registerController } from '~/controllers/users.controllers';
-import { loginValidator } from '~/middlewares/users.middlewares';
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares';
 const usersRouter = Router();
 
 usersRouter.post('/login', loginValidator, loginController);
-usersRouter.post('/register', registerController);
+
+/**
+ * Register a new user
+ * Path: /users/register
+ * POST
+ * Body{
+ * name: string,
+ * MSV: string,
+ * email: string,
+ * password: string,
+ * confirmPassword: string,
+ * date_of_birth: ISO8601,
+ * }
+ */
+usersRouter.post('/register', registerValidator, registerController);
 export default usersRouter;
