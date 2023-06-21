b299739cb531da751ce3d9b311776c684ec26dae
lantuyan <nqlan.20it4@vku.udn.vn>
2023-06-22 02:39:22 +0700

    create access token, refresh token when register

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,18 +1,55 @@
 import { Request, Response } from 'express';
+import { TokenType } from '~/constants/enum';
 import { RegisterRequestBody } from '~/models/requests/User.requests';
 import User from '~/models/schemas/User.schema';
 import databaseService from '~/services/database.services';
+import { hashPassword } from '~/utils/crypto';
+import { signToken } from '~/utils/jwt';
 
 class UsersService {
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.AccessToken
+      },
+      options: {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+      }
+    });
+  }
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.RefreshToken
+      },
+      options: {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+      }
+    });
+  }
+
   async register(payload: RegisterRequestBody) {
     // const { email, password } = payload;
     const result = await databaseService.users.insertOne(
       new User({
         ...payload,
-        date_of_birth: new Date(payload.date_of_birth)
+        date_of_birth: new Date(payload.date_of_birth),
+        password: hashPassword(payload.password)
       })
     );
-    return result;
+
+    const user_id = result.insertedId.toString();
+    const [access_token, refresh_token] = await Promise.all([
+      this.signAccessToken(user_id),
+      this.signRefreshToken(user_id)
+    ]);
+
+    return {
+      access_token,
+      refresh_token
+    };
   }
 
   async checkEmailExist(email: string) {
